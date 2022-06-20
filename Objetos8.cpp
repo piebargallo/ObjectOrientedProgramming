26f5c20f53e9a3f5e65ca5134e4bfb7f8e547318
piebargallo <90567646+piebargallo@users.noreply.github.com>
2022-06-20 18:27:20 +0200

    Add files via upload

---
+++ Objetos8.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+class Car {  				// The class
+	public:  				// Access specifier
+		string brand;		// Attribute
+		string model;		// Attribute
+		int year;			// Attribute
+		Car(string x, string y, int z); // Constructor declaration
+}; // End class
+
+// Constructor definition outside the class
+Car::Car(string x, string y, int z) {
+	brand = x;
+	model = y;
+	year = z;
+} // End constructor
+
+// Driver program
+int main() {
+	// Create Car objects and call the constructor with different values
+	Car carObj1("BMW", "X5", 1999);
+	Car carObj2("Ford", "Munstang", 1969);
+	
+	// Print values
+	cout << carObj1.brand << " " << carObj1.model << " " << carObj1.year << "\n";
+	cout << carObj2.brand << " " << carObj2.model << " " << carObj2.year << "\n";
+	
+	return 0;
+} // End driver program
